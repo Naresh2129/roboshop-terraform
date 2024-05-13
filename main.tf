68c06c78f18b7bd8614f7109cc9d1552cd6954b8
Naresh2129 <nareshboligorla@gmail.com>
2024-05-13 21:20:12 +0530

    subnets creation

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 module "components" {
   source = "https://github.com/Naresh2129/tf-module-vpc.git"
+
   for_each = var.vpc
-  cidr = each.value[cidr]
+  cidr = each.value["cidr"]
+  subnets = each.value["subnets"]
+
+}
+
+output "vpc" {
+  value = module.vpc
 }
