dbed7f40b29ca13ad2925171c63bf9214ac578a1
Naresh2129 <nareshboligorla@gmail.com>
2024-05-29 17:29:01 +0530

    tags

--- main.tf
+++ main.tf
@@ -108,4 +108,7 @@ module "app" {
   sg_ingress_cidr = local.app_subnets_cidr
   vpc_id = local.vpc_id
   subnet_ids = local.app_subnets
+
+  alb_name  = lookup(lookup(module.alb, "private", null), "dns_name", null)
 }
+
