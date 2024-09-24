6653ac160fa5f39197da21a671da2338ac117f04
naresh <nareshboligorla@gmail.com>
2024-09-24 15:43:33 +0530

    creation of vpc

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
   module "vpc" {
-  source = "https://github.com/Naresh2129/tf-module-vpc.git"
+  source = "git::https://github.com/Naresh2129/tf-module-vpc.git"
 
   for_each = var.vpc
   cidr     = each.value["cidr"]
