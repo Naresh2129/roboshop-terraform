ac97c313ff6c32405eb6895bbb87692d4f60dc6f
naresh <nareshboligorla@gmail.com>
2024-12-12 08:50:43 +0530

    creation of subnets

--- main.tf
+++ main.tf
@@ -2,4 +2,9 @@ module "vpc" {
   source = "git::https://github.com/Naresh2129/tf-module-vpc.git"
   for_each = var.vpc
   cidr = each.value["cidr"]
+  subnets = each.value["subnets"]
+
+  module"subnets"
+     source = "./subnets"
+     vpc_id = var.aws_vpc.main.id
 }
